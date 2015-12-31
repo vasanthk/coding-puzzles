ab1a5f88a8a349d3b0032bea5fa5198cae4f21a2
Vasa <vasanth.krishnamoorthy@citrix.com>
2015-12-30 22:23:59 -0800

    Added note + link

--- js/squareRoot.js
+++ js/squareRoot.js
@@ -1,14 +1,19 @@
 /**
  * Implement a function to find the square root of a number, without using math.sqrt();
  *
+ * Hint: Binary Search
+ *
  * @Reference:
  * https://github.com/EvanSkeete/front-end-interviews/blob/master/challenges.js#L143
+ * http://www.geeksforgeeks.org/square-root-of-an-integer/
+ *
+ * Time Complexity: O(log n)
  */
 
 function sqrt(n) {
   var start = 0;
   var end = n / 2;
-  var epsilon = 0.00001;
+  var epsilon = 0.00001;  // This is the precision to which we want the returned value to be accurate.
 
   var choice = (end - start) / 2;
   var result = choice * choice;
