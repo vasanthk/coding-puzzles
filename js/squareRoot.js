7941bd5e89ce1a73f7a908e0f3242d43cd6baa45
Vasa <vasanth.krishnamoorthy@citrix.com>
2016-01-11 14:03:50 -0800

    Simple binary search for sqrt

--- js/squareRoot.js
+++ js/squareRoot.js
@@ -11,6 +11,40 @@
  */
 
 function sqrt(n) {
+  if (n === 0 || n === 1) {
+    return n;
+  }
+
+  var start = 1;
+  var end = n;
+  var ans;
+  var mid;
+
+  while (start <= end) {
+    mid = (start + end) / 2;
+
+    // If n is a perfect square
+    if (mid * mid === n) {
+      return mid;
+    }
+
+    // Since we need floor, we update answer when mid*mid is
+    // smaller than n, and move closer to sqrt(n)
+    if (mid * mid < n) {
+      start = mid + 1;
+      ans = mid;
+    } else {
+      // if mid * mid > n
+      end = mid - 1;
+    }
+  }
+  return ans;
+}
+
+
+// Alternative Soln: More precise, but hangs for few nums eg. sqrt(3) etc
+// TODO: Fix this version
+function sqrt(n) {
   var start = 0;
   var end = n / 2;
   var epsilon = 0.00001;  // This is the precision to which we want the returned value to be accurate.
