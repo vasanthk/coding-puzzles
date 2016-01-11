5289856a532669b66f68901758eb9d713361d626
Vasa <vasanth.krishnamoorthy@citrix.com>
2016-01-10 23:20:03 -0800

    Reverse string

---
+++ ctci/Arrays and Strings/2.reverseString.js
@@ -0,0 +1,27 @@
+/**
+ * Reversing a string
+ *
+ * @Reference:
+ * http://eddmann.com/posts/ten-ways-to-reverse-a-string-in-javascript/
+ * http://ilikekillnerds.com/2015/07/reversing-a-string-in-javascript-3-different-ways/
+ * ES6: http://stackoverflow.com/a/34445777/1672655
+ */
+
+// Basic for loop
+function reverseString(str) {
+  var op = '';
+  for (var i = str.length - 1; i >= 0; i--) {
+    op += str[i];
+  }
+  return op;
+}
+
+// Using inbuilt fn
+function reverseString(str) {
+  return str.split('').reverse().join('');
+}
+
+// ES6
+function reverseString(str) {
+  return [...str].reverse().join('');
+}
