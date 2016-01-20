c0ee785f2c5f59b6e9f79d0de887fd72d58b5b52
Vasa <vasanth.krishnamoorthy@citrix.com>
2016-01-20 01:31:13 -0800

    First common ancestor - Non BST

---
+++ ctci/trees-and-graphs/7.firstCommonAncestorNonBST.js
@@ -0,0 +1,49 @@
+/**
+ * Design an algorithm and write code to find the first common ancestor
+ * of two nodes in a binary tree. Avoid storing additional nodes in a data structure.
+ * NOTE: This is not necessarily a binary search tree.
+ *
+ * @Reference:
+ * https://github.com/shreeya/cci-javascript/blob/master/4.7.js
+ */
+
+var Tree = function (value) {
+  this.left;
+  this.right;
+  this.value = value;
+};
+
+function findCommonAncestor(root, nodeA, nodeB) {
+  if (doesPathExist(root.left, nodeA) && doesPathExist(root.left, nodeB)) {
+    //they are both on the left side -> look on the left side
+    return findCommonAncestor(root.left, nodeA, nodeB);
+  } else if (doesPathExist(root.right, nodeA) && doesPathExist(root.right, nodeB)) {
+    //they are both on the right side -> look on the right side
+    return findCommonAncestor(root.right, nodeA, nodeB);
+  } else {
+    //they are on opposite sides -> this is the common ancestor
+    return root;
+  }
+}
+
+function doesPathExist(parent, node) {
+  if (parent === undefined) {
+    return false;
+  } else {
+    if (parent === node) {
+      return true;
+    } else {
+      return doesPathExist(parent.left, node) || doesPathExist(parent.right, node);
+    }
+  }
+}
+
+var t = new Tree(1);
+t.left = new Tree(2);
+t.right = new Tree(3);
+t.left.left = new Tree(4);
+t.left.right = new Tree(5);
+t.right.left = new Tree(6);
+t.right.right = new Tree(7);
+
+console.log(findCommonAncestor(t, t.left.left, t.right));
