54c3d9d71c6ce1756645b227389867c96ecf01ac
Vasa <vasanth.krishnamoorthy@citrix.com>
2016-02-03 00:20:41 -0800

    Method to return all subsets of set -- more notes

--- ctci/recursion-and-dp/4.subsetsOfSet.js
+++ ctci/recursion-and-dp/4.subsetsOfSet.js
@@ -1,4 +1,8 @@
 /**
+ * Write a method to return all subsets of a set.
+ *
+ * Time Complexity: O(2^n)
+ * Space Complexity: O(2^n)
  *
  * @Reference:
  * https://github.com/weihotline/CrackingTheCodingInterviewJavaScript/blob/master/08_chp8/03_subsets.js
@@ -9,6 +13,8 @@ function subsets(array) {
     return [[]]; // empty set
   }
 
+  // Main Logic: (Pg 322 in book)
+  // subset(n) = subset(n-1) + Each array in subset(n-1) with current new element.
   var oldSet = subsets(array.slice(1));
   var newSet = oldSet.map(function (sub) {
     var newSub = sub.concat();
