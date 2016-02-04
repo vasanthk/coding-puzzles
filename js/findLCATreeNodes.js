577d581e4464182cbb5658a4d4157d4b3fb0d107
Vasa <vasanth.krishnamoorthy@citrix.com>
2016-02-04 00:03:20 -0800

    LCA reference links

--- js/findLCATreeNodes.js
+++ js/findLCATreeNodes.js
@@ -2,6 +2,9 @@
  * Find LCA between two nodes in binary tree
  *
  * Hint: Break it down to sub-problems (Dynamic Programming)
+ *
+ * LCA for Binary Search Tree: http://articles.leetcode.com/2011/07/lowest-common-ancestor-of-a-binary-search-tree.html
+ * LCA for Binary Tree: http://articles.leetcode.com/2011/07/lowest-common-ancestor-of-a-binary-tree-part-i.html
  */
 
 function findLCATreeNodes(node1, node2, tree) {
