b449f8c5e1732497e26608f5642bb4626479ed4f
Vasa <vasanth.krishnamoorthy@citrix.com>
2016-02-09 21:43:16 -0800

    Remove duplicates from array

---
+++ js/removeDuplicatesFromArray.js
@@ -0,0 +1,23 @@
+/**
+ * Remove duplicates from Array
+ *
+ * @Reference:
+ * http://codereview.stackexchange.com/questions/60128/removing-duplicates-from-an-array-quickly
+ */
+
+Array.prototype.unique = function (mutate) {
+  var unique = this.reduce(function (accum, current) {
+    if (accum.indexOf(current) < 0) {
+      accum.push(current);
+    }
+    return accum;
+  }, []);
+  if (mutate) {
+    this.length = 0;
+    for (var i = 0; i < unique.length; ++i) {
+      this.push(unique[i]);
+    }
+    return this;
+  }
+  return unique;
+};
