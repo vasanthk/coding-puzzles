17836a14532d1fae8fc78b8022cb59ef3c8daca7
Vasa <vasanth.krishnamoorthy@citrix.com>
2016-02-14 23:01:01 -0800

    All combinations

--- js/combinations.js
+++ js/combinations.js
@@ -3,8 +3,32 @@
  *
  * @Reference:
  * http://stackoverflow.com/a/4061198/1672655
+ * https://gist.github.com/axelpale/3118596
+ *
  */
 
+// Returns all combinations
+function combinations(arr, active) {
+  if (!active) {
+    active = '';
+  }
+
+  if (arr.length == 0) {
+    console.log(active);
+  } else {
+    combinations(arr.slice(1), active + arr[0]);
+    // For string combinations
+    // combinations(rest.substring(1, rest.length), active + rest.charAt(0))
+
+    combinations(arr.slice(1), active);
+    // For string combinations
+    // combinations(rest.substring(1, rest.length), active)
+  }
+}
+
+// console.log(combinations([1,2,3]));
+
+// Returns all combinations of length `n`
 function combinations(numArr, choose) {
   var res = [];
 
