d568539fc0b27908b2719fe1bc81dbc93c2ba465
Vasa <vasanth.krishnamoorthy@citrix.com>
2016-02-17 21:41:38 -0800

    Removed unneeded comments

--- js/findUnrepeatedNumber.js
+++ js/findUnrepeatedNumber.js
@@ -1,11 +1,6 @@
 /**
- * Given an array of numbers - find the unrepeated number.
- *
- * @Reference:
- *
+ * Find the one unrepeated number in an array of numbers that repeat even number of times
  */
-
-// Find the one unrepeated number in an array of numbers that repeat even number of times
 
 function findUnrepeatedNumber(arr) {
   return arr.reduce(function(num, result) {
